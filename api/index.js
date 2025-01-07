b2d1a04e858f9e9a70e1a6be0e192fe590ccf9a5
William Silva <williamsilva20062005@gmail.com>
2025-01-07 15:48:06 -0300

    feat: add CORS support to API and update package dependencies

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // Adicione esta linha
 const { initDb } = require("../src/config/database");
 const verifyApiKey = require("../src/middlewares/authMiddleware");
 const taskRoutes = require("../src/routes/taskRoutes");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "x-api-key"],
+  })
+);
 
 initDb().catch(console.error);
 
